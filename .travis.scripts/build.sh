7d15442ed030e064ef2e1b152d6eecd4bf679e70
Ivan De Marino <detronizator@gmail.com>
2018-03-31 21:14:02 +0100

    I think I almost got cross compilation for Linux, Windows and MacOSX at 32 and 64 bits working

--- .travis.scripts/build.sh
+++ .travis.scripts/build.sh
@@ -4,4 +4,4 @@ ${RUST_BUILD_TOOL} build --target ${RUST_TARGET} --release
 
 ls -lh target/
 ls -lh target/${RUST_TARGET}/release
-file target/${RUST_TARGET}/release/httpbox
+file target/${RUST_TARGET}/release/httpbox*
