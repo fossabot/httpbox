40d1d2905edf8f122ae507424e67c74522479d26
Ivan De Marino <detronizator@gmail.com>
2018-03-31 20:21:21 +0100

    Still messing around with cross compilation

--- .travis.scripts/setup.sh
+++ .travis.scripts/setup.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+echo "Travis OS: ${TRAVIS_OS_NAME}"
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+    echo "Installing dependencies via 'homebrew'"
+    brew update
+    brew install tree
+
     echo "Updating rustup"
     rustup self update
 
     echo "Setting up build target: ${TARGET}"
     rustup target add ${TARGET}
 else
-    echo "Installing cross"
-    cargo install cross
+    echo "Installing dependencies via 'APT'"
+    apt-get -qq update -qq
+    apt-get -qqy install tree
+
+    echo "Installing cross (if absent) (see https://github.com/japaric/cross)"
+    cargo install --list | grep cross || cargo install cross
 fi
